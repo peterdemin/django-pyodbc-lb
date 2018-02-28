caa90f6c543428975cd32298f0c85b36c516c074
Petr <petr.demin@nih.gov>
2018-02-28 10:01:29 -0500

    bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ CLASSIFIERS=[
 
 setup(
     name='django-pyodbc-lb',
-    version='1.11.0.0',
+    version='1.11.0.1',
     description='Django backend for Microsoft SQL Server with load balancer support',
     long_description=open('README.rst').read(),
     author='Petr Demin',
